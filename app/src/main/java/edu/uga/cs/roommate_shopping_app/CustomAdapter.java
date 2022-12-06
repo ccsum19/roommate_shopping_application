91328ff0caba7ae5c671a8ab28f1c4dca9072f05
Sumin Cho <suminyj@sogang.ac.kr>
2022-12-06 11:57:22 -0500

    final modified

--- app/src/main/java/edu/uga/cs/roommate_shopping_app/CustomAdapter.java
+++ app/src/main/java/edu/uga/cs/roommate_shopping_app/CustomAdapter.java
@@ -166,12 +166,11 @@ public class CustomAdapter extends BaseAdapter {
                             alertDialogBuilder.setPositiveButton("OK",
                                     new DialogInterface.OnClickListener(){
                                         public void onClick(DialogInterface dialog, int which) {
-
+                                            dialog.dismiss();
                                         }
                                     });
                             AlertDialog alertDialog = alertDialogBuilder.create();
                             alertDialog.show();
-                            dao.setListStatus(houseKey, listKey, "purchased");
                         }
                     });
                 }
